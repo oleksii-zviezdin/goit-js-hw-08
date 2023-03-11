9486455ac2fff03d930b928dd60861303220b41b
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-03-12 00:49:01 +0100

    ended tast 8.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
+
+const iframeEl = document.querySelector('iframe');
+
+const player = new Player(iframeEl);
+
+const STORAGE_CURRENT_TIME = "videoplayer-current-time";
+const currentTime = localStorage.getItem(STORAGE_CURRENT_TIME);
+
+if (currentTime !== null) {
+    player.setCurrentTime(currentTime);
+}
+
+player.on(`timeupdate`, throttle(getCurrentTime, 1000) )
+
+function getCurrentTime(data){
+    try {
+        localStorage.setItem(STORAGE_CURRENT_TIME, data.seconds);
+    } catch (e) {
+        console.error('Failed to save current time to local storage:', e);
+    }
+}
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
+
+const formEl = document.querySelector(`form`);
+const FEEDBACK_KEY = "feedback-form-state";
+
+formEl.addEventListener(`input`, throttle(setData, 500));
+formEl.addEventListener(`submit`, removeData);
+
+const feedbackData = {};
+
+const dataFromLocalStorage = localStorage.getItem(FEEDBACK_KEY);
+const parseDataFromLocalStorage = JSON.parse(dataFromLocalStorage);
+
+const emailInputEl = document.querySelector(`input[type="email"]`);
+const messageInputEl = document.querySelector(`textarea[name="message"]`);
+
+const emailUser = parseDataFromLocalStorage.email;
+emailInputEl.value = emailUser ? emailUser : ``;
+
+const messageUser = parseDataFromLocalStorage.message;
+messageInputEl.value = messageUser ? messageUser : ``;
+
+function setData(e) {
+    feedbackData[e.target.name] = e.target.value;
+    localStorage.setItem(FEEDBACK_KEY, JSON.stringify(feedbackData))
+}
+
+function removeData(e) {
+    console.log(parseDataFromLocalStorage);
+    localStorage.removeItem(FEEDBACK_KEY);
+    e.target.reset();
+}
+
 
